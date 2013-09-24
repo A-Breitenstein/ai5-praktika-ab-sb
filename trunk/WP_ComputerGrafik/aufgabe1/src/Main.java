d09ade2bf8f4ad065720d9a29739acbeb5681bd2
sven.bartel90 <sven.bartel90@1af27a68-00ba-3aab-f193-c8eb602027ec>
2013-09-24 20:12:39 +0000

    jetzt aber

---
+++ trunk/WP_ComputerGrafik/aufgabe1/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: abg667
+ * Date: 24.09.13
+ * Time: 12:37
+ * To change this template use File | Settings | File Templates.
+ *
+ */
+public class Main {
+}
