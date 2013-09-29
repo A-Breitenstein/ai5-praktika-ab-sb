85c37d33095f14e4deebf3cd1823df1953e61e6d
breitenstein.alexander <breitenstein.alexander@googlemail.com>
2013-09-29 21:47:21 +0000

    kommentare

--- VS/src/aufgabe1/server.erl
+++ VS/src/aufgabe1/server.erl
@@ -4,23 +4,23 @@
 
 %% API
 -export([]).
-
-%lol ka vergessen aber nachricht einfügen in den pool
-addToPool(Message) ->
-  MessagePool = [MessagePool, Message].
-
-%% giveMessages(Id) ->
-%% ... liste von tupeln aus (reader, MessagePoolIndex)
-% nach 10 readertupeln wird der erste verdrängt oder so
-% bei abfrage wird gezählt im MessagePool und ab index rausgegeben
-
-loop() ->
-  receive
-    {"writer", Message} ->
-      addToPool(Message)
-        ;
-
-    {"reader", Id} ->
-      Id ! giveMessages(Id)
-
-  end.
+%%
+%% %lol ka vergessen aber nachricht einfügen in den pool
+%% addToPool(Message) ->
+%%   MessagePool = [MessagePool, Message].
+%%
+%% %% giveMessages(Id) ->
+%% %% ... liste von tupeln aus (reader, MessagePoolIndex)
+%% % nach 10 readertupeln wird der erste verdrängt oder so
+%% % bei abfrage wird gezählt im MessagePool und ab index rausgegeben
+%%
+%% loop() ->
+%%   receive
+%%     {"writer", Message} ->
+%%       addToPool(Message)
+%%         ;
+%%
+%%     {"reader", Id} ->
+%%       Id ! giveMessages(Id)
+%%
+%%   end.
