8066e37fd3d3a1ad988f89594f5c751ccda14333
sven.bartel90 <sven.bartel90@1af27a68-00ba-3aab-f193-c8eb602027ec>
2013-10-26 18:28:16 +0000

    soweit fertig implementiert, alle compile errors gefixt, noch keinen testlauf durchgeführt

--- VS/src/aufgabe2/messages.erl
+++ VS/src/aufgabe2/messages.erl
@@ -10,4 +10,26 @@
 -author("abg628").
 
 %% API
--export([]).
+-export([sendConnect/3, sendInitiate/5, enqueue/2, sendReport/3]).
+
+%% Enqueues the given MSG in NodeNames process messagequeue
+enqueue(NodeName,MSG) ->
+  Node = global:whereis_name(NodeName),
+  Node ! MSG
+.
+
+
+sendInitiate(TargetNodeName,Level,FragmentName,State,Edge)->
+ enqueue(TargetNodeName,{initiate,Level,FragmentName,State,Edge})
+.
+
+sendConnect(TargetNodeName,Level, Edge)->
+  enqueue(TargetNodeName,{connect,Level,Edge})
+.
+
+sendReport(TargetNodeName,BestWeight,BestEdge) ->
+  enqueue(TargetNodeName,{report,BestWeight,BestEdge})
+.
+sendAccept(TargetNodeName,Edge)->
+  enqueue(TargetNodeName,{accept,Edge})
+.
