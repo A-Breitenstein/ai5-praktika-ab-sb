b505c942e856f7217eaba55dc5b5d2cd60dde90e
timolange.tl@googlemail.com <timolange.tl@googlemail.com@1af27a68-00ba-3aab-f193-c8eb602027ec>
2013-11-25 21:30:30 +0000

    -problem mit registry behoben, anscheinend kann man nur eine registry erstellen. -throws RemoteException fehlt bei allen signaturen im IMpsServer interface

--- trunk/AI/src/hawmps/komponenten/server/MpsServer.java
+++ trunk/AI/src/hawmps/komponenten/server/MpsServer.java
@@ -2,6 +2,7 @@ package hawmps.komponenten.server;
 
 import aufgabe1.persistence.PersistenceUtilsA1;
 import hawmps.fassade.HawMps;
+import hawmps.starter.Config;
 
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -18,10 +19,6 @@ import java.rmi.server.UnicastRemoteObject;
 public class MpsServer extends HawMps implements IMpsServer{
     private String serverName;
     private HeartBeat heartBeat;
-    public static final int SERVER_REGISTRY_PORT = Registry.REGISTRY_PORT; // standard RegistryPort
-    public static final int HEARTBEAT_REGISTRY_PORT = Registry.REGISTRY_PORT + 1; // standard RegistryPort plus 1
-    public static final String REGISTRY_HOST = "localhost"; //ist selbe adresse wie die des dispatchers
-
 
     private MpsServer(String serverName, HeartBeat heartBeat) {
         super(PersistenceUtilsA1.createEntityManager());
@@ -34,7 +31,7 @@ public class MpsServer extends HawMps implements IMpsServer{
         MpsServer mpsSever = new MpsServer(serverName, heartBeat);
         IMpsServer stub = (IMpsServer)UnicastRemoteObject.exportObject(mpsSever, 0); //Damit wird der Server in die RMI-Runtime eingetragen, praktisch die server schleife
 
-        Registry registry = LocateRegistry.getRegistry(REGISTRY_HOST, SERVER_REGISTRY_PORT); //holt sich die entfernte registry
+        Registry registry = LocateRegistry.getRegistry(Config.REGISTRY_HOST, Config.REGISTRY_PORT); //holt sich die entfernte registry
         registry.rebind(serverName, stub); //treagt den MpsServer unter dem ServerName in die Registry ein
 
         heartBeat.start();
---
+++ trunk/AI/src/hawmps/starter/Config.java
@@ -0,0 +1,16 @@
+package hawmps.starter;
+
+import java.rmi.registry.Registry;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: timey
+ * Date: 25.11.13
+ * Time: 21:41
+ * To change this template use File | Settings | File Templates.
+ */
+public class Config {
+    public static final int REGISTRY_PORT = Registry.REGISTRY_PORT; // standard RegistryPort
+    public static final String REGISTRY_HOST = "localhost"; //ist selbe adresse wie die des dispatchers
+    public static final String MONITOR_NAME = "monitor";
+}
