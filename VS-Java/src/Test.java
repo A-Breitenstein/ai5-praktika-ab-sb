12a41f91d4fb1a0e8008a26ecb31669443ee4d81
breitenstein.alexander <breitenstein.alexander@1af27a68-00ba-3aab-f193-c8eb602027ec>
2013-11-27 14:25:22 +0000

    Project-Import

---
+++ VS-Java/src/Test.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: abg628
+ * Date: 27.11.13
+ * Time: 15:23
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+}
