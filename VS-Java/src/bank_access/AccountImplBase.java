895df3f14dac3c99bb6b9b81a482d6fd39252055
breitenstein.alexander <breitenstein.alexander@googlemail.com>
2013-11-29 00:52:02 +0000

    added: Beispiele für Dynamische erzeugung von SkeletonFactory Objekten durch JReflections oder Interface und super Delegation

--- VS-Java/src/bank_access/AccountImplBase.java
+++ VS-Java/src/bank_access/AccountImplBase.java
@@ -13,4 +13,7 @@ public abstract class AccountImplBase {
     public abstract void transfer(double amount) throws OverdraftException;
     public abstract double getBalance();
     public static AccountImplBase narrow_cast(Object o) {return new AccountImplStub((NameServiceMessage) o);}
+
+    //TEST
+    public static SkeletonFactoryBase skeletonFactory(){ return new AccountSkeletonFactory();}
 }
