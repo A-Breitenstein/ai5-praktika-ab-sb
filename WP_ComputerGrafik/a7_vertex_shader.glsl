38583214b10162490229436659d5d597e8a817e5
sven.bartel90 <sven.bartel90@1af27a68-00ba-3aab-f193-c8eb602027ec>
2013-12-09 22:13:20 +0000

    aufgabe7 lol....

--- WP_ComputerGrafik/a7_vertex_shader.glsl
+++ WP_ComputerGrafik/a7_vertex_shader.glsl
@@ -1,7 +1,8 @@
-varying vec2 texture_coordinate;
-
+//varying vec2 texture_coordinate;
+varying vec3 N;
+varying vec3 E;
 void main(){
-
+     /*
     gl_Position = ftransform();
 
     gl_TexCoord[0] = gl_MultiTexCoord0;
@@ -14,4 +15,16 @@ void main(){
 
     gl_TexCoord[1].s = r.x/m + 0.5;
     gl_TexCoord[1].t = r.y/m + 0.5;
+
+    */
+
+       	// Normale vom Vertex berechnen
+       	N = normalize(gl_NormalMatrix * gl_Normal);
+    	// Richtung von der Kamera (Betrachter (E)) zum Vertex.
+        vec3 v = vec3(gl_ModelViewMatrix * gl_Vertex);
+    	// Richtung zur Kamera (Betrachter (E)) normalisiert
+        E = normalize(-v);
+
+       	// Compute position in 3-space.
+       	gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
 }
