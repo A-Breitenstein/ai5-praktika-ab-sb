904b4678df106f2761433eb864cf37b519ca3887
breitenstein.alexander <breitenstein.alexander@1af27a68-00ba-3aab-f193-c8eb602027ec>
2013-12-17 14:51:35 +0000

    shaderdateien

--- WP_ComputerGrafik/a7_fragment_shader.glsl
+++ WP_ComputerGrafik/a7_fragment_shader.glsl
@@ -1,23 +1,21 @@
-/*
-uniform sampler2D colorMap;
-uniform sampler2D envMap;
-*/
+
 varying vec3 N;
 varying vec3 E;
 uniform sampler2D envMap;
 
 void main (void)
 {
-	/*
-	vec4 color = texture2D( colorMap, gl_TexCoord[0].st);
-	vec4 env = texture2D( envMap, gl_TexCoord[1].st);
 
-	gl_FragColor = color + env*0.4;
-	*/
+    float pi = 3.14159265;
 	vec3 R = normalize(reflect(E,N));
-	float x_winkel = (3.14159265 + atan(R.y,R.x))/(2.0*3.14159265);
-	float z_winkel = atan(sqrt(R.x*R.x + R.y*R.y),R.z)/3.14159265;
+	float x_winkel = (pi + atan(R.y,R.x)) / (2.0*pi);
+	float z_winkel = atan(sqrt(R.x*R.x + R.y*R.y),R.z) / pi;
+
+    //TEST
+	if(x_winkel > 0.5){x_winkel = 1;z_winkel = 1;}else{x_winkel = 0.1;z_winkel = 0.1;}
+
     vec2 texCoords = vec2(x_winkel,z_winkel);
+
     vec4 color = texture2D(envMap,texCoords);
    	gl_FragColor = color;
 
