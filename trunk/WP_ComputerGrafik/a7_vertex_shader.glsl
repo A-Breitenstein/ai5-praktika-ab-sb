3b937874397f106e8b28a626380849f1e2da2d0b
breitenstein.alexander <breitenstein.alexander@1af27a68-00ba-3aab-f193-c8eb602027ec>
2013-12-17 14:51:35 +0000

    shaderdateien

--- trunk/WP_ComputerGrafik/a7_vertex_shader.glsl
+++ trunk/WP_ComputerGrafik/a7_vertex_shader.glsl
@@ -1,22 +1,7 @@
-//varying vec2 texture_coordinate;
 varying vec3 N;
 varying vec3 E;
 void main(){
-     /*
-    gl_Position = ftransform();
 
-    gl_TexCoord[0] = gl_MultiTexCoord0;
-
-    vec3 u = normalize(vec3(gl_ModelViewMatrix * gl_Vertex));
-    vec3 n = normalize(gl_NormalMatrix * gl_Normal);
-    vec3 r = reflect(u,n);
-
-    float m = 2.0 * sqrt(r.x*r.x + r.y*r.y + (r.z+1.0)*(r.z+1.0));
-
-    gl_TexCoord[1].s = r.x/m + 0.5;
-    gl_TexCoord[1].t = r.y/m + 0.5;
-
-    */
 
        	// Normale vom Vertex berechnen
        	N = normalize(gl_NormalMatrix * gl_Normal);
@@ -25,6 +10,7 @@ void main(){
     	// Richtung zur Kamera (Betrachter (E)) normalisiert
         E = normalize(-v);
 
-       	// Compute position in 3-space.
-       	gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+        // Compute position in 3-space.
+        gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+
 }
